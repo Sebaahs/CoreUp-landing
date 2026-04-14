293396d6c531f8f2c37117f456ea6836db03f8dc
Sebastian Dipietro <sebastianleonel2002@gmail.com>
2026-04-14 16:32:01 -0300

    privacidad

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   base: '/',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        privacidad: resolve(__dirname, 'privacidad/index.html')
+      }
+    }
   },
   plugins: [
     tailwindcss(),
